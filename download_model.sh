2515c20c9583a72df4a1d644cd45a7343850adb5
elrohil <w.stanek123@gmail.com>
2019-11-10 16:29:55 +0100

    Updated model in order to be compliant with downgraded keras

--- download_model.sh
+++ download_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-fileid="1XNAotDX55yM_rkFkk0-XfljA9CDnk9G9"
+fileid="1u9xW76XTDs1p9AjAnAQv_h7MEl9wInJU"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -o- -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" | tar xz
