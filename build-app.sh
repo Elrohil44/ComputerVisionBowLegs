91bd4e24a78bd0c71cb6b69a892d5ed502fab1d2
elrohil <w.stanek123@gmail.com>
2020-02-01 12:04:00 +0100

    Fixed app building

--- build-app.sh
+++ build-app.sh
@@ -8,4 +8,4 @@ STATIC_RESOURCES_ROOT_URL=http://${EC2_INSTANCE_IP}/static
 docker build -t elrohil/bow-legs-app:latest -t elrohil/bow-legs-app:${VERSION} \
     --build-arg "REACT_APP_API_URL=${API_URL}" \
     --build-arg "REACT_APP_STATIC_RESOURCES_ROOT_URL=${STATIC_RESOURCES_ROOT_URL}" \
-    "./$1"
+    "./app"
